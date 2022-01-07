a3d707fd2bde00a69744d5dec063f5ea96009a2a
Nicolas Mendes <nicolasmendes_developer@outlook.com>
2022-01-07 18:06:17 -0300

    Adding some conversion scripts

---
+++ Conversion Scripts/celciusToFahrenheit.js
@@ -0,0 +1,3 @@
+let celciusVal = 21;
+const fahrenheitVal = (celciusVal * 9 / 5) + 32;
+console.log("The temperature in Fahrenheit is " + fahrenheitVal + ".");
---
+++ Conversion Scripts/hoursToSeconds.js
@@ -0,0 +1,3 @@
+let hoursV = 8;
+const secondsV = hoursV * 3600;
+console.log("The number of seconds in " + hoursV + " hours is " + secondsV + ".");
