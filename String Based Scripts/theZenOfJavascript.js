cdcb53256bad724c9412fc56a6999b2caf01efb4
Nicolas Mendes <nicolasmendes_developer@outlook.com>
2022-01-12 16:17:12 -0300

    Adding Zen of javascript script

---
+++ String Based Scripts/theZenOfJavascript.js
@@ -0,0 +1,17 @@
+let zen = `
+Zen Of Javascript
+ 
+Build functionality, not architecture.
+Build small things that work
+and glue them together to make big things.
+Clear is better than clever,
+    but graceful beats both.
+Comments are good, even if you're Dutch.
+Asychronous is better than synchronous
+except for when it isn't
+but your code should work on both.
+Convention is better than definition,
+if you have good docs.
+Keep it loose, but build good tests.`;
+
+console.log(zen);
